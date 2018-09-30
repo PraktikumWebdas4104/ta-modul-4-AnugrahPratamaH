632421d4df376f5f1ebb698367fc52a23b253fc4
Anugrah Pratama H <43516625+AnugrahPratamaH@users.noreply.github.com>
2018-09-30 15:16:59 +0700

    Update formawal.php

--- formawal.php
+++ formawal.php
@@ -1,5 +1,9 @@
-<?php
+ <form action="" method="POST">
+	Username : <input type="text" name="username"><br><br>
+	Password : <input type="password" name="password"><br><br>
+	<input type="submit" name="submit" value="kirim">                                                                                                             
 
-
-
-?>
+</form>
+<?php 
+	include 'prosess.php';
+	?> 
